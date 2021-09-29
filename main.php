6e8ade905169734363179b365f30b9a3c7a85e9e
Meghana Sripalle <88918638+MeghanaSripalle@users.noreply.github.com>
2021-09-29 17:03:34 +0530

    Add files via upload

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "user";
-$password = "pass";
+$username = "root";
+$password = "";
 
 $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
@@ -12,71 +12,73 @@ $querydb = "CREATE DATABASE Library";
 $conn->query($querydb);
 
 $dbname = "Library";
+$conn = new mysqli($servername, $username, $password,$dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
 
 $queryadmin = "CREATE TABLE Admins(
-  Id UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  Username varchar(30) UNIQUE NOT NULL,
-  Password varchar(30) UNIQUE NOT NULL
+  Id int(2) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  Username varchar(30)  UNIQUE NOT NULL,
+  Password varchar(30)  UNIQUE NOT NULL
 )";
 
 $conn->query($queryadmin);
 
 $queryad = "INSERT INTO Admins (Username,Password)
-VALUES ('sudeshna','january')";
-$queryad .= "INSERT INTO Admins (Username,Password)
-VALUES ('sahithy','november')";
-$queryad .= "INSERT INTO Admins (Username,Password)
-VALUES ('meghana','december')";
-$queryad .= "INSERT INTO Admins (Username,Password)
-VALUES ('tamal','ssl')";
+VALUES ('sudeshna','january'),
+('sahithy','november'),
+('meghana','december'),
+('tamal','ssl')";
 
-$conn->multi_query($queryad);
+$conn->query($queryad);
 
 
 $querystudents = "CREATE TABLE Students(
     Id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    UserId int(9) NOT NULL UNIQUE,
-    UserPassword varchar(30) NOT NULL UNIQUE
+    UserId int(9) NOT NULL ,
+    UserPassword varchar(30) NOT NULL )
     ";
 
 $conn->query($querystudents);
 
-$queryst = "INSERT INTO Students (UserId,User)
-VALUES ('200010001','rohan')";
-$queryst .= "INSERT INTO Students (UserId,User)
-VALUES ('200010002','mary')";
-$queryst .= "INSERT INTO Students (UserId,User)
-VALUES ('200010003','julie')";
-$queryst .= "INSERT INTO Students (UserId,User)
-VALUES ('200010004','ria')";
-$queryst .= "INSERT INTO Students (UserId,User)
-VALUES ('200010005','meghana')";
-$queryst .= "INSERT INTO Students (UserId,User)
-VALUES ('200010006','sahithy')";
-$queryst .= "INSERT INTO Students (UserId,User)
-VALUES ('200010007','sudeshna')";
-$queryst .= "INSERT INTO Students (UserId,User)
-VALUES ('200010008','tamal')";
-$queryst .= "INSERT INTO Students (UserId,User)
-VALUES ('200010009','chandler')";
-$queryst .= "INSERT INTO Students (UserId,User)
-VALUES ('200010010','monica')";
+$queryst = "INSERT INTO Students (UserId,UserPassword)
+VALUES ('200010001','rohan'),
+('200010002','mary'),
+('200010003','julie'),
+('200010004','ria'),
+('200010005','meghana'),
+('200010006','sahithy'),
+('200010007','sudeshna'),
+ ('200010008','tamal'),
+('200010009','chandler'),
+('200010010','monica')";
 
-$conn->multi_query($queryst);
+if($conn->query($queryst) == TRUE){
+  echo "Students is created.<br>";
+}else{
+  echo "No".$conn->error;
+}
 
 $querybooks = "CREATE TABLE Books(
-  BookId int(6 )AUTO_INCREMENT UNIQUE UNSIGNED PRIMARY KEY,
-  Title varchar(120) NOT NULL,
-  Quantity int(6) UNSIGNED,
+  BookId int(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  Title varchar(120),
+  Quantity int(6) ,
   Author varchar(30) NOT NULL,
   Publisher varchar(50) NOT NULL,
   Genre varchar(20) 
 )";
 
-$conn->query($querybooks);
+if($conn->query($querybooks)==FALSE){
+    echo $conn->error;
+}
 
 $querybook = "INSERT INTO Books (Title,Quantity,Author,Publisher,Genre)
-VALUES ('Thousand Splendid Suns','10','Khaled Hosseini','Bloomsbury',) ";
+VALUES ('Thousand Splendid Suns','10','Khaled Hosseini','Bloomsbury','Literary Fiction'), 
+('All the light we cannot see','5','Anthony','Harper Collins','Literary Fiction')";
+$conn->query($querybook);
+
 
 
 

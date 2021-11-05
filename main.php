a1e22eb4c580eab30c0c938bff28caa7970e984e
Meghana Sripalle <88918638+MeghanaSripalle@users.noreply.github.com>
2021-11-06 05:22:48 +0530

    updated with html

--- main.php
+++ main.php
@@ -37,8 +37,8 @@ $conn->query($queryad);
 
 $querystudents = "CREATE TABLE Students(
     Id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    UserId int(9) NOT NULL ,
-    UserPassword varchar(30) NOT NULL )
+    UserId int(9) UNIQUE NOT NULL ,
+    UserPassword varchar(30) UNIQUE NOT NULL )
     ";
 
 $conn->query($querystudents);
@@ -57,22 +57,22 @@ VALUES ('200010001','rohan'),
 
 if($conn->query($queryst) == TRUE){
   echo "Students is created.<br>";
-}else{
-  echo "No".$conn->error;
 }
+
 
 $querybooks = "CREATE TABLE Books(
   BookId int(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  Title varchar(120),
+  Title varchar(120) UNIQUE,
   Quantity int(6) ,
-  Author varchar(30) NOT NULL,
-  Publisher varchar(50) NOT NULL,
-  Genre varchar(20) 
+  Author varchar(120) NOT NULL,
+  Publisher varchar(120) NOT NULL,
+  Genre varchar(50) 
 )";
 
-if($conn->query($querybooks)==FALSE){
-    echo $conn->error;
-}
+$conn->query($querybooks);
+
+
+
 
 $querybook = "INSERT INTO Books (Title,Quantity,Author,Publisher,Genre)
 VALUES ('Thousand Splendid Suns','10','Khaled Hosseini','Bloomsbury','Literary Fiction'),
@@ -99,14 +99,14 @@ VALUES ('Thousand Splendid Suns','10','Khaled Hosseini','Bloomsbury','Literary F
 ('War and Peace','10','Leo Tolstoy','Vintage','Historical fiction'),
 ('The Silence of the Lambs','10','Thomas Harris','St. Martin''s Press','Thriller'),
 ('A Game of Thrones','10','George R. R. Martin','Bantam Spectra','Fantasy'),
-('Adventures of Huckleberry Finn','10','Mark Twain','Chatto & Windus / Charles L. Webster And Company','Literary realism'),
+('Adventures of Huckleberry Finn','10','Mark Twain','Charles L. Webster And Company','Literary realism'),
 ('Great Expectations','10','Charles Dickens','Chapman & Hall','Bildungsroman'),
 ('The Time Machine','10','H. G. Wells','William Heinemann','Science Fiction'),
 ('Apples Never Fall','10','Liane Moriarty','Holt, Henry & Company, Inc.','Thriller'),
 ('The Glass Castle','10','Jeannette Walls','Scribner','Memoir'),
 ('Milk and honey','10','Rupi Kaur','Andrews McMeel Publishing','Poetry'),
 ('The 48 Laws of Power','10','Robert Greene','hg book centre','Self-help'),
-('The Sun and Her Flowers','10','Rupi Kaur','Andrews McMeel Publishing','Poetry')
+('The Sun and Her Flowers','10','Rupi Kaur','Andrews McMeel Publishing','Poetry'),
 ('Pride and Prejudice','5','Jane Austen','Penguin Publications','Classics'),
  ('Jane Eyre','15','Charlotte Bronte','Penguin Publications','Classics'),
  ('Kashi','6','Vineet Bajpai','Treeshade books','Fiction'),
@@ -117,7 +117,7 @@ VALUES ('Thousand Splendid Suns','10','Khaled Hosseini','Bloomsbury','Literary F
  ('Pralay','6','Vineet Bajpai','Treeshade Books','Fiction'),
  ('I''ll give you the sun','10','Jandy Nelson','Dial Books','Fiction'),
  ('Norwegian Wood','10','Haruki Murakami','Kodansha','Fiction'),
- ('Never let me go','7','Kazuo Ishiguro','Faber and Faber','speculative fiction'),
+ ('Never let me go','7','Kazuo Ishiguro','Faber and Faber','Speculative Fiction'),
  ('Where the Crawdads Sing','10','Delia Owens','G.P Putnam''s Sons','Literary fiction'),
  ('Eleanor Oliphant is completely fine','4','Gail Honeyman','Harper Collins','Fiction'),
  ('Discrete Mathematics and Its Applications','30','Kenneth Rosen','McGraw Hill','Discrete Structures'),
@@ -128,28 +128,48 @@ VALUES ('Thousand Splendid Suns','10','Khaled Hosseini','Bloomsbury','Literary F
  ('Introduction to Algorithms, Third Edition','60','Thomas H. Cormen','The MIT Press','Data Structures and Algorithms'),
  ('Data Structures and Algorithms Made Easy in Java','35','Narasimha Karumanchi','Careermonk Publications','Data Structures and Algorithms'),
  ('Data structures and algorithms in C++:3rd Edition','45','Adam Drozdek','Thomson Press','Data Structures and Algorithms'),
- ('Data Structures and Algorithms in Python','50','Michael H. Goldwasser','Wiley','Data Structures and Algorithms')
- ('Algorithms Unlocked','55','Thomas H. Cormen','The MIT Press','Data Structures and Algorithms')
- ('The Algorithm Design Manual','43','Steven S. Skiena','Springer Publications','Data Structures and Algorithms')
+ ('Data Structures and Algorithms in Python','50','Michael H. Goldwasser','Wiley','Data Structures and Algorithms'),
+ ('Algorithms Unlocked','55','Thomas H. Cormen','The MIT Press','Data Structures and Algorithms'),
+ ('The Algorithm Design Manual','43','Steven S. Skiena','Springer Publications','Data Structures and Algorithms'),
 ('Advanced Data Structures','60','Peter Brass','Cambridge Press','Data Structures and Algorithms'),
-('Cracking the Coding Interview: 189 Programming Questions and Solutions 6th Edition','100','Gayle Laakmann McDowell','CareerCup','Data Structures and Algorithms')
+('Cracking the Coding Interview: 189 Programming Questions and Solutions 6th Edition','100','Gayle Laakmann McDowell','CareerCup','Data Structures and Algorithms'),
 ('Principles of Macroeconomics','50','N. Gregory Mankiw','Cengage Learning','Economics'),
 ('Intermediate Microeconomics: A Modern Approach','50','Hal Varian','WW Norton & Co','Economics'),
-('Microeconomic Theory: Basic Principles and Extensions','50','Walter Nicholson','Economics'),
+('Microeconomic Theory: Basic Principles and Extensions','50','Walter Nicholson','The MIT Press','Economics'),
 ('Microeconomic theory','40','Andreu Mas-Colell','Oxford University Press','Economics'),
 ('Microeconomics: Principles, Problems, and Policies','30','Campbell McConnell','McGraw-Hill Education','Economics'),
-('Microeconomic Foundations I: Choice and Competitive Markets','31','David M. Kreps','Princeton University Press ','Economics'),
+('Microeconomic Foundations I: Choice and Competitive Markets','31','David M. Kreps','Princeton University Press','Economics'),
 ('Microeconomics: Theory and Applications with Calculus ','25','Jeffrey Perloff','Pearson','Economics'),
 ('Advanced Macroeconomics Fifth Edition','60','David Romer','McGraw-Hill Education','Economics'),
 ('Linux Pocket Guide, Third Edition','50','Daniel J. Barrett','O''Reilly Media','Software Systems Lab'),
 ('Linux Command Line and Shell Scripting Bible','36','Christine Bresnahan','Wiley India Pvt Ltd','Software Systems Lab'),
 ('Linux System Programming','55','Robert Love','O''Reilly Media','Software Systems Lab'),
-('The Linux Command Line, 2nd Edition: A Complete Introduction','47','William Shotts','No Starch Press (RHPS)','Software Systems Lab'),
+('The Linux Command Line, 2nd Edition: A Complete Introduction','47','William Shotts','No Starch Press','Software Systems Lab'),
 ('Version Control with Git','60','Jon Loeliger','O''Reilly Media','Software Systems Lab'),
 ('Git Pocket Guide: A Working Introduction','60','Richard E. Silverman','O''Reilly Media','Software Systems Lab'),
-('Professional Git','30','Brent Laster','O'Reilly Media','Software Systems Lab'),
+('Professional Git','30','Brent Laster','O''Reilly Media','Software Systems Lab')
 ";
 $conn->query($querybook);
 
+
 $conn->close();
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Welcome to IIT DHARWAD Library Management System</title>
+        <link rel="stylesheet" href="login.css"> 
+</head>
+    
+<body> 
+<div style="height: 100px;">
+        <h1 style="text-align: center;color: rgb(85, 55, 21);"> WELCOME TO OUR LIBRARY MANAGEMENT SYSTEM</h1></div>
+        <div class="container">
+        <div class="vertical-center"><button style="border: 10%;border-color:rgb(88, 60, 17);background-color: rgb(231, 212, 192);" class="slogin"><a href="loginstudent.php">Login as a Student</a></button></div>
+        <div class="vertical-center1"><button style="border: 10%;border-color:rgb(88, 60, 17);background-color: rgb(231, 212, 192);" class="slogin"><a href="loginadmin.php">Login as an Admin</a></button></div>
+        </div>
+
+
+        
+</body>
+</html>

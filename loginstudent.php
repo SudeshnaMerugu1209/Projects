c231cb229ed6ef8488c0315a9b96eec8fb16d96a
Meghana Sripalle <88918638+MeghanaSripalle@users.noreply.github.com>
2021-11-06 06:28:00 +0530

    updated final

--- loginstudent.php
+++ loginstudent.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login Page</title>
-    <style>
-
-    </style>
+    <title>Student Login Page</title>
+    <link rel="stylesheet" href="loginstudent.css">
+    
 </head>
 
-<body style="text-align: center">
+<body>
+    <div class="container">
+    <div><button style="background-color: rgb(107, 80, 45);color: rgb(77, 45, 22);font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS',sans-serif"><a href="main.php">Back</a></button></div>
+        <h1 style="text-align: center;color: antiquewhite;text-indent: 20px;background-color: rgb(77, 45, 22);width: 100%;">Student Login</h1>
+        <div class="container1">
     <form method="post" action="loginstudent.php">
-        User ID : <input type="number" name="userid" id="userid" min="9" max="9"><br><br>
-        Password : <input type="password" name="pass" id="pass" ><br><br>
-        <input type="submit" value="Login" name="submitstudent" id="submitstudent">
+        <div class="vertical-center">User ID : <input type="number" name="userst" id="userst" min="200000001" max="200030060"><br><br></div>
+       <div class="vertical-center1">Password : <input type="password" name="passst" id="passst" ><br><br></div>
+       <div class="vertical-center2"><input style="color: rgb(77, 45, 22);font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;background-color:rgb(172, 145, 126) ;"type="submit" value="Login" name="submitstudent" id="submitstudent"></div>
     </form>
+    </div>
+    </div>
 </body>
 </html>
